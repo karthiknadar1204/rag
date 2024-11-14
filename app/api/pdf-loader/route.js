8efc0287300c500c9e1c70c3ecdfd389c8dc2eb6
karthik <karthiknadar1204@gmail.com>
2024-11-14 15:15:58 +0530

    convert data to embeddings and store in vector stores

--- app/api/pdf-loader/route.js
+++ app/api/pdf-loader/route.js
@@ -2,9 +2,15 @@ import { NextResponse } from "next/server";
 import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
-const pdfUrl="https://pleasant-gecko-979.convex.cloud/api/storage/6752d390-ce23-49ed-9d8b-b72c428b34e0";
+// const pdfUrl="https://pleasant-gecko-979.convex.cloud/api/storage/6752d390-ce23-49ed-9d8b-b72c428b34e0";
 
 export async function GET(req,res){
+
+    const reqUrl=req.url;
+    const {searchParams}=new URL(reqUrl);
+    const pdfUrl=searchParams.get('pdfUrl');
+    console.log(pdfUrl);
+
     //1. Load pdf file from url.
     const response=await fetch(pdfUrl);
     const data=await response.blob();

579bf53b6ed7e83c0d4a2ae7a3732c9d07933328
karthik <karthiknadar1204@gmail.com>
2024-11-14 08:57:13 +0530

    app setup and convex db and basic auth

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      page
+      <Button>Dabao Mujhe</Button>
+
+    </div>
+  )
+}
+
+export default page
